464ec95be5f488c955c1cfdfff3c8608d1db3a4e
Ana Lam <alam@barnard.edu>
2021-06-15 16:41:29 -0400

    follow up question of parent co

--- yourstake.py
+++ yourstake.py
@@ -21,6 +21,7 @@ def search_query_URL(base_URL, tables, reporting_year, output_fileformat):
         tables: String. Name of tables with relevant data we want to pull.
         reporting_year: Int. Desired year of report.
         output_fileformat: String. Desired output file format.
+    Output: Search query URL
 
     Note: The URL output limit is 100000 rows at a time, would need to create a for loop
     to pull all rows. We can generate the count for the table by changing output_fileformat to "COUNT"
@@ -32,6 +33,9 @@ def search_query_URL(base_URL, tables, reporting_year, output_fileformat):
 def content_to_pandas(URL):
     """
     Fetch content from URL and convert to pandas dataframe.
+    Arguments:
+        URL: String. Search query URL.
+    Output: Pandas Dataframe of CSV data
     """
     r = requests.get(URL).content
     df = pd.read_csv(io.StringIO(r.decode('utf-8')), engine="python",
@@ -39,6 +43,7 @@ def content_to_pandas(URL):
     if len(df.columns) <= 1:
         print("Error in EPA URL.")
     else:
+        print("EPA data extracted.")
         return df
 
 EPA_df = content_to_pandas(search_query_URL(base_URL,
@@ -72,26 +77,69 @@ naics_dict = dict(zip(naics_df["2017 NAICS US Code"], naics_df["2017 NAICS US Ti
 EPA_df["industry_code(EPA)"] = EPA_df["industry_code(EPA)"].astype(str).map(naics_dict)
 EPA_df = EPA_df.rename(columns={"industry_code(EPA)":"industry"})
 
-
+# Replace nan values for parent company with facility name
+EPA_df.parent_company_name.fillna(EPA_df.facility_name, inplace=True)
 
 ## EEA, no API (would either do more research on potential resources or have selenium web bot download data)
 
-"""
-## With Selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('/Users/alam/Desktop/chromedriver')
+import time
+
+# instead of searching for filename, pull the EEA download from most recent downloads in Chrome
+def getDownLoadedFileName(waitTime):
+    """
+    Get file name of downloaded file.
+    Arguments:
+        waitTime: Int. Number of seconds to wait for download.
+    Output: filename
+    Note: The URL output limit is 100000 rows at a time, would need to create a for loop
+    to pull all rows. We can generate the count for the table by changing output_fileformat to "COUNT"
+    and pulling rows in 100000 chunks with for loop.
+    """
+    driver.execute_script("window.open()") # switch to new tab
+    driver.switch_to.window(driver.window_handles[-1]) # navigate to chrome downloads
+    driver.get('chrome://downloads')
+    endTime = time.time()+waitTime
+    while True:
+        try:
+            # get downloaded percentage
+            downloadPercentage = driver.execute_script(
+                "return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value")
+            if downloadPercentage == 100:
+                # return the file name once the download is completed
+                return driver.execute_script("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content  #file-link').text")
+        except:
+            pass
+        time.sleep(1)
+        if time.time() > endTime:
+            break
+
+options = webdriver.ChromeOptions()
+prefs = {'download.default_directory' : os.getcwd()} # set download directory to current working directory
+options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome('/Users/alam/Desktop/chromedriver', options=options)
+
+print('Extracting EEA data...')
 
 driver.get(
     'https://www.eea.europa.eu/data-and-maps/data/industrial-reporting-under-the-industrial-3')
 table_link = driver.find_element_by_id('1d6eee54f9fc43729e208c9e72c63237') # link to EEA data download page
 
 driver.execute_script("arguments[0].click();", table_link) # trigger download link
-"""
 
-EEA_df = pd.read_csv("E-PRTR_Pollutant_Releases.csv", low_memory=False)
-EEA_df = EEA_df.loc[(EEA_df['reportingYear'] == 2019) & (EEA_df['medium'] == "AIR")]
+from zipfile import ZipFile
+
+file_name = getDownLoadedFileName(10)
+
+with ZipFile(file_name, 'r') as zip:  # extract files from downloaded Zipfile
+    zip.extractall()
+
+EEA_df = pd.read_excel(r"E-PRTR Pollutant Releases.xlsx")
+print('EEA data extracted.')
+
+EEA_df = EEA_df.loc[(EEA_df['reportingYear'] == 2019) & (EEA_df['medium'] == "AIR")] # filter for reporting year and toxic air releases
 
 
 columns_of_interest = ["parentCompanyName", "nameOfFeature", "mainActivityName",
@@ -108,12 +156,20 @@ EEA_df = EEA_df.rename(columns={"nameOfFeature":"facility_name",
                         "totalPollutantQuantityKg":"total_air_pollutants_kg",
                         "mainActivityName":"industry"})
 
-combined_df = pd.concat([EPA_df, EEA_df], axis=0)
+combined_df = pd.concat([EPA_df, EEA_df], axis=0) # aggregate EPA and EEA reported toxic air pollution for 2019
 combined_df.reset_index(drop=True, inplace=True)
 
 
 combined_df.to_csv("EPA_EEA_table.csv")
 
+# Create CSV of parent company toxic air pollution releases
+parentco_df = EPA_df.groupby('parent_company_name')['total_air_pollutants_kg'].sum().reset_index()
+parentco_df = pd.concat([parentco_df, EEA_df.groupby('parent_company_name')['total_air_pollutants_kg'].sum().reset_index()], axis=0)
+
+parentco_df.reset_index(drop=True, inplace=True)
+parentco_df.to_csv("EPA_EEA_parentco_table.csv")
+
 
 if __name__ == "__main__":
     print("EPA and EEA combined CSV written at \yourstake\EPA_EEA_table.csv")
+    print("Parent company total toxic air pollution releases CSV written at \yourstake\EPA_EEA_parentco_table.csv")
